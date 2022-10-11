968d4323265eb03dba2064d43984d91c74417c59
Giuseppe <88315256+JIMMI74@users.noreply.github.com>
2022-10-11 12:26:13 +0200

    Add files via upload

---
+++ finale.js
@@ -0,0 +1,9 @@
+//selezioniamo nodi dal DOM
+const navToggle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+//evento sul bottone navToggle che aggiunge/elimina la classe show-links
+navToggle.addEventListener("click", function () {
+  
+  navLinks.classList.toggle("show-links");
+});
